f462f65c6126a39bfcf18258809ece06ea6981d5
Chris Polansky <cpolansky@hashicorp.com>
2025-12-08 19:53:55 +0000

    Adding fqdn output - map with fqdn

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "fqdns" {
-  value = [ for i in range(var.node_count): { "${i}" = "${openstack_compute_instance_v2.nodes[i].access_ip_v4}" } ]
+  value = [ for i in range(var.node_count): { "${local.fqdns[i]}" = "${openstack_compute_instance_v2.nodes[i].access_ip_v4}" } ]
 }
