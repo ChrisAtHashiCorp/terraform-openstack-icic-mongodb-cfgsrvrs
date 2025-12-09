f4134d55fe7fa0f9e4e9fa3d14c49f7af0eeabf8
Chris Polansky <cpolansky@hashicorp.com>
2025-12-09 07:47:25 +0000

    Fixing map

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "fqdns" {
-  value = [ for i in range(var.node_count): { "${local.fqdns[i]}" = "${openstack_compute_instance_v2.nodes[i].access_ip_v4}" } ]
+  value = { for i, k in local.fqdns : k => "${openstack_compute_instance_v2.nodes[i].access_ip_v4}" }
 }
 
 output "cluster_id" {
