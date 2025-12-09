59cd29784b8f7f013fe7c274e850e52aa945a9d2
Chris Polansky <cpolansky@hashicorp.com>
2025-12-09 07:13:05 +0000

    Adding cluster_id output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "fqdns" {
   value = [ for i in range(var.node_count): { "${local.fqdns[i]}" = "${openstack_compute_instance_v2.nodes[i].access_ip_v4}" } ]
 }
+
+output "cluster_id" {
+  value = "cfg-${random_id.cluster_id.hex}"
+}
